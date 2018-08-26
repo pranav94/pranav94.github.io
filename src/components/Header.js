aca17736eae75ef6afa6b42ba1d3f5d990c99ca5
Pranav <pranavudupa94@gmail.com>
2018-08-26 21:32:47 +0530

    Fix name

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Icon, Button } from 'antd'
+import Texty from 'rc-texty';
+import TweenOne from 'rc-tween-one'
 
 import logo from '../logo.svg'
-import Texty from 'rc-texty';
-import TweenOne from 'rc-tween-one';
 
-class Demo extends React.Component {
+export default class Header extends React.Component {
     state = {
         show: true,
         collapsed: false,
@@ -165,7 +165,7 @@ class Demo extends React.Component {
                                     ],
                                 }}
                             >
-                                Pranav
+                                Pranav Udupa S
                         </Texty>
                             <TweenOne
                                 className='combined-bar'
@@ -186,76 +186,5 @@ class Demo extends React.Component {
                 </div>
             </div>
         );
-    }
-}
-
-export default class Header extends React.Component {
-    state = {
-        collapsed: false,
-    }
-    toggleCollapsed = () => {
-        this.setState({
-            collapsed: !this.state.collapsed,
-        })
-    }
-    render() {
-        return (
-            <div>
-                {/* <div className={'sidebar ' + (this.state.collapsed ? 'open' : 'close')}>
-                    <Link to='/'>
-                        <img height='60' src={logo} alt='P.U' />
-                    </Link>
-                    <div>
-                        <Link onClick={this.toggleCollapsed} to='/'>
-                            Home
-                        </Link>
-                    </div>
-                    <div>
-                        <Link onClick={this.toggleCollapsed} to='/experience'>
-                            Experience
-                        </Link>
-                    </div>
-                    <div>
-                        <Link onClick={this.toggleCollapsed} to='/contact'>
-                            Contact
-                        </Link>
-                    </div>
-                </div>
-                <div className='mobilemenu'>
-                    <Button type='primary' style={{ marginLeft: this.state.collapsed ? '32vw' : 0 }} onClick={this.toggleCollapsed}>
-                        <Icon type={this.state.collapsed ? 'menu-fold' : 'menu-unfold'} />
-                    </Button>
-                </div>
-                <div className='title'>
-                    <img height='80' src={logo} alt='P.U' />
-                    <div className='navbar'>
-                        <span className='links'>
-                            <span>
-                                <Link to='/'>
-                                    Home
-                                </Link>
-                            </span>
-                            <span>
-                                <Link to='/experience'>
-                                    Experience
-                                </Link>
-                            </span>
-                            <span>
-                                <Link to='/contact'>
-                                    Contact
-                                </Link>
-                            </span>
-                        </span>
-                    </div>
-                    <div className='intro'>
-                        <h1>
-                            Pranav Udupa
-                        </h1>
-                    </div>
-                </div>
-                <img className='photo' height='80' src='./images/photo.jpg' alt='P.U' /> */}
-                <Demo />
-            </div>
-        )
     }
 }
